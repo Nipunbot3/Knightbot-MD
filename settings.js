d67e5c54a1293f556a9ede873bd5ea95fbe7d768
Nipunbot3 <122987917+Nipunbot3@users.noreply.github.com>
2025-02-27 17:46:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Udara Sampath', // Your name
+  ownerNumber: '+94 77 358 0827', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a Devilz bot for managing group commands and automating task.Rep by Sri Lanka",
   version: "1.0.0",
 };
 
